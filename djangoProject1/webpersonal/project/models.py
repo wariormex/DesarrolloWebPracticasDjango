3b7347ac07db7d41abf9c493bc581898c3cc1061
wariormex <91165602+wariormex@users.noreply.github.com>
2022-09-09 16:46:33 -0500

    Update models.py

--- djangoProject1/webpersonal/project/models.py
+++ djangoProject1/webpersonal/project/models.py
@@ -5,7 +5,7 @@ class Project(models.Model):
     title = models.CharField(max_length=100, verbose_name="Titulo")
     description = models.TextField(verbose_name="Descripcion")
     image = models.ImageField(verbose_name="Imagen", upload_to="project")
-    url = models.URLField(verbose_name="URL")
+    url = models.URLField(null=True, blank=True, verbose_name="URL")
     
     created = models.DateTimeField(auto_now_add=True,verbose_name="Fecha de creacion")
     updated = models.DateTimeField(auto_now=True,verbose_name="Fecha de modificacion")
